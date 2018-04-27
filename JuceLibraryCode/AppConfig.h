499bb26787052e14922004beb156358bb32677a8
Richard Yang <richard40148@gatech.edu>
2018-04-27 03:29:12 -0400

    clean up unused dependency

--- JuceLibraryCode/AppConfig.h
+++ JuceLibraryCode/AppConfig.h
@@ -333,7 +333,7 @@
  #define JucePlugin_ManufacturerCode       0x4d616e75 // 'Manu'
 #endif
 #ifndef  JucePlugin_PluginCode
- #define JucePlugin_PluginCode             0x5762306e // 'Wb0n'
+ #define JucePlugin_PluginCode             0x55766275 // 'Uvbu'
 #endif
 #ifndef  JucePlugin_IsSynth
  #define JucePlugin_IsSynth                0
