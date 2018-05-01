9e60e68b379b6861fd21ee9b44a621071f129485
Richard Yang <richard40148@gatech.edu>
2018-04-30 23:05:41 -0400

    solving ghost directory issue

--- prerender/prerender.h
+++
@@ -1,4 +0,0 @@
-/* (Auto-generated binary data file (for parsing wavetables into matrix)). */
-/*index method: Saw = 0 , Sin 100, Tri 200, Saw = 300(repeat)*/
-
-extern double prerenderWabetable[303][512];
