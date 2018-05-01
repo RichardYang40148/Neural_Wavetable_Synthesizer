a7dd0841ccac890945ec4df0de28bfcc37dd7768
Richard Yang <richard40148@gatech.edu>
2018-04-30 23:06:21 -0400

    update prerender files

---
+++ prerender/prerender.h
@@ -0,0 +1,4 @@
+/* (Auto-generated binary data file (for parsing wavetables into matrix)). */
+/*index method: Saw = 0 , Sin 100, Tri 200, Saw = 300(repeat)*/
+
+extern double prerenderWabetable[303][512];
